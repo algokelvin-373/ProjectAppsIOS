ca78b33387adfcd2dce92778e2e70974ffa0d3cd
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-04-13 05:23:42 +0700

    001: Hello World iOS ( Using SwiftUI )

---
+++ M01_Hello_World/m01_Hello_World/m01_Hello_World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m01_Hello_World
+//
+//  Created by Kelvin HT on 4/13/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
