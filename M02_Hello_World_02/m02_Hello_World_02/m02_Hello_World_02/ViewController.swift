7d2b488ff890983065d023ee7fefb1a026ea6c9e
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-04-13 05:28:18 +0700

    002: Hello World iOS 02 ( Using Storyboard )

---
+++ M02_Hello_World_02/m02_Hello_World_02/m02_Hello_World_02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m02_Hello_World_02
+//
+//  Created by Kelvin HT on 4/13/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
