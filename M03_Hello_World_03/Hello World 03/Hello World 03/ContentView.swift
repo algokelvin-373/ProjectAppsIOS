6929e48c4f23232d1d946f0b972dda3e87ad3ac3
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-14 15:51:45 +0700

    003: Hello World Class Dicoding iOS Beginner

---
+++ M03_Hello_World_03/Hello World 03/Hello World 03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World 03
+//
+//  Created by Kelvin HT on 4/14/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
