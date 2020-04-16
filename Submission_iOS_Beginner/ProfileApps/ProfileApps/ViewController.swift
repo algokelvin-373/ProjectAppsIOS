38b02f9fa5c1f8cf3d362d6fcca01163b72bb6c3
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-16 23:00:25 +0700

    Submission iOS Beginner: ProfileApps ( Make New Project )

---
+++ Submission_iOS_Beginner/ProfileApps/ProfileApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProfileApps
+//
+//  Created by Kelvin HT on 4/16/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
