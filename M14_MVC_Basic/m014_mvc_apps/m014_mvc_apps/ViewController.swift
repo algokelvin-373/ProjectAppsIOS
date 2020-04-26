74afcb415fca37e718075ea0aff5c080209ba5fe
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-27 04:54:34 +0700

    Add Module Training MVC Controller

---
+++ M14_MVC_Basic/m014_mvc_apps/m014_mvc_apps/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  m014_mvc_apps
+//
+//  Created by Kelvin HT on 4/25/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtName: UILabel!
+    @IBOutlet weak var txtJob: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+}
