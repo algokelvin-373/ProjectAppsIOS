1760916b4374da29992e6b75b21a7f382da1aafa
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-28 00:48:11 +0700

    Success Get Data Profile using implement MVC

--- Submission_iOS_Beginner/ProfileApps/ProfileApps/ViewController.swift
+++ Submission_iOS_Beginner/ProfileApps/ProfileApps/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textShortDescription: UILabel!
+    @IBOutlet weak var textLongDescription: UILabel!
+    @IBOutlet weak var textAbout: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let profile = getDataProfile()
+        
+        textAbout.text = profile.name
+        textAbout.attributedText = NSAttributedString(
+            string: textAbout.text ?? "empty_text",
+            attributes: setBoldText(sizeFont: 20.0)
+        )
+        
+        textLongDescription.text = profile.long_description
+        textShortDescription.text = profile.short_description
+        
     }
 }
 
---
+++ Submission_iOS_Beginner/ProfileApps/ProfileApps/model/Profile.swift
@@ -0,0 +1,25 @@
+//
+//  Profile.swift
+//  ProfileApps
+//
+//  Created by Kelvin HT on 4/27/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import Foundation
+
+class Profile {
+    var name: String?
+    var amountPost: String?
+    var amountFollowers: String?
+    var amountFollow: String?
+    
+    var email: String?
+    var date_of_birth: String?
+    var place_of_birth: String?
+    var address_house: String?
+    var address: String?
+    
+    var short_description: String?
+    var long_description: String?
+}
