2810150d705f6084aecb496a2583509d9c61fb31
kelvin373-ht <kelvin373.ht@gmail.com>
2020-05-02 00:02:25 +0700

    Make function go to detail Data Language Programming ( Crash Apps )

--- M13_Dynamic_TableView/m13_dynamic_tableview/m13_dynamic_tableview/DetailLanguageProgrammingViewController.swift
+++ M13_Dynamic_TableView/m13_dynamic_tableview/m13_dynamic_tableview/DetailLanguageProgrammingViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class DetailLanguageProgrammingViewController: UIViewController {
 
+    var language_programming: LanguageProgramming?
+    
+    @IBOutlet weak var titleDetailLanguageProgramming: UILabel!
+    @IBOutlet weak var imageDetailLanguageProgramming: UIImageView!
+    @IBOutlet weak var descriptionDetailLanguageProgramming: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let result = language_programming {
+            titleDetailLanguageProgramming.text = result.title
+            imageDetailLanguageProgramming.image = result.image
+            descriptionDetailLanguageProgramming.text = result.description
+        }
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
