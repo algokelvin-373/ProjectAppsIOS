68af90d6e9f381382fdf57d524b7384d5b28d902
kelvin373-ht <kelvin373.ht@gmail.com>
2020-05-01 18:59:50 +0700

    Add Module 13: Dynamic Table

---
+++ M13_Dynamic_TableView/m13_dynamic_tableview/m13_dynamic_tableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m13_dynamic_tableview
+//
+//  Created by Kelvin HT on 5/1/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
