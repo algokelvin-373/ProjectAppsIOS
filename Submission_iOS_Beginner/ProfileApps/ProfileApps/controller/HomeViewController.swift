667fa21dcf118ba1d3431bf6568c68c57ac1dc7e
kelvin373-ht <kelvin373.ht@gmail.com>
2020-05-05 20:35:00 +0700

    Develop Home Page ( Status Photos in TableView )

---
+++ Submission_iOS_Beginner/ProfileApps/ProfileApps/controller/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  ProfileApps
+//
+//  Created by Kelvin HT on 5/3/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableStatusPhotos: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableStatusPhotos.dataSource = self
+        tableStatusPhotos.register(
+            UINib(nibName: "StatusPhotosTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "StatusPhotosCell"
+        )
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        statusPhotos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StatusPhotosCell", for: indexPath) as! StatusPhotosTableViewCell
+        
+        let status = statusPhotos[indexPath.row]
+        cell.imageStatus.image = status.image
+        cell.titleStatus.text = status.title
+        
+        return cell
+    }
+}
+
