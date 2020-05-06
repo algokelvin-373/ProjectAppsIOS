12f09a8c962bc4f0583e3f62c28701c3074165db
kelvin373-ht <kelvin373.ht@gmail.com>
2020-05-06 14:58:19 +0700

    Add Detail Status Photos Page

---
+++ Submission_iOS_Beginner/ProfileApps/ProfileApps/DetailStatusViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailStatusViewController.swift
+//  ProfileApps
+//
+//  Created by Kelvin HT on 5/6/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import UIKit
+
+class DetailStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
