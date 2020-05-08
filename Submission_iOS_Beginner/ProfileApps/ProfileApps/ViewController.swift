68abd545a149198867ddc259cffa9832b67a4d9c
kelvin373-ht <kelvin373.ht@gmail.com>
2020-05-09 01:41:18 +0700

    Settings AutoLayout 02

--- Submission_iOS_Beginner/ProfileApps/ProfileApps/ViewController.swift
+++ Submission_iOS_Beginner/ProfileApps/ProfileApps/ViewController.swift
@@ -10,23 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textShortDescription: UILabel!
-    @IBOutlet weak var textLongDescription: UILabel!
-    @IBOutlet weak var textAbout: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let profile = getDataProfile()
+//        let profile = getDataProfile()
         
-        textAbout.text = profile.name
-        textAbout.attributedText = NSAttributedString(
-            string: textAbout.text ?? "empty_text",
-            attributes: setBoldText(sizeFont: 20.0)
-        )
-        
-        textLongDescription.text = profile.long_description
-        textShortDescription.text = profile.short_description
+//        textAbout.text = profile.name
+//        textAbout.attributedText = NSAttributedString(
+//            string: textAbout.text ?? "empty_text",
+//            attributes: setBoldText(sizeFont: 20.0)
+//        )
+//
+//        textLongDescription.text = profile.long_description
         
     }
 }
