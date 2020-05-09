1200c069bdad7ce9f1b5238f7f388f30eeacc5a0
kelvin373-ht <kelvin373.ht@gmail.com>
2020-05-09 16:03:37 +0700

    Set Data Profile

---
+++ Submission_iOS_Beginner/ProfileApps/ProfileApps/controller/DataPostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DataPostViewController.swift
+//  ProfileApps
+//
+//  Created by Kelvin HT on 5/9/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import UIKit
+
+class DataPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
