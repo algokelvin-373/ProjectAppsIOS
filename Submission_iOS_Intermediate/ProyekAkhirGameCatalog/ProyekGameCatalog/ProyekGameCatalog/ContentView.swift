2b5ffd038587d3b0af5162fd30db897ca0c57e2f
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-01 21:38:28 +0700

    Add New Project Proyek Game Catalog with Testing

---
+++ Submission_iOS_Intermediate/ProyekAkhirGameCatalog/ProyekGameCatalog/ProyekGameCatalog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProyekGameCatalog
+//
+//  Created by Kelvin HT on 7/1/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
