c2012bb5b8008942556f751e23597ee658b2bebf
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-03 22:03:09 +0700

    Set type Font: largeTitle, FontWeight: bold, ForegroundColor: black, and Padding all with 8.0

--- M15_UI_Text_Button_TextField/m15_ui_text_button_textfield/m15_ui_text_button_textfield/ContentView.swift
+++ M15_UI_Text_Button_TextField/m15_ui_text_button_textfield/m15_ui_text_button_textfield/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello AlgoKelvin")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(Color.black)
+            .padding(.all, 8.0)
     }
 }
 
