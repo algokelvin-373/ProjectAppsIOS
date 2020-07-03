ef6c8e75b32d8a5f6d105ebedcda8bf1bd022330
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-03 21:56:48 +0700

    Add Module 015: UI Text Button TextField

---
+++ M15_UI_Text_Button_TextField/m15_ui_text_button_textfield/m15_ui_text_button_textfield/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m15_ui_text_button_textfield
+//
+//  Created by Kelvin HT on 7/3/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
