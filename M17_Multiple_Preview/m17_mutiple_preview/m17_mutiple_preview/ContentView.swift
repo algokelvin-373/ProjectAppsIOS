d83152b12aeec3103276d8af817159296ed703ae
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-04 20:52:11 +0700

    Add Module 017: Mutiple Preview

---
+++ M17_Multiple_Preview/m17_mutiple_preview/m17_mutiple_preview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m17_mutiple_preview
+//
+//  Created by Kelvin HT on 7/4/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
