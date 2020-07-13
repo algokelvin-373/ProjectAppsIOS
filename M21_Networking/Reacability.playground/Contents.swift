a4e382b879c91e96a61600f7b577e32ea7bba23c
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-13 12:47:33 +0700

    Networking: Reacability ( Done )

--- M21_Networking/Reacability.playground/Contents.swift
+++ M21_Networking/Reacability.playground/Contents.swift
@@ -1,3 +1,11 @@
-import Cocoa
 
-var str = "Hello, playground"
+import SystemConfiguration
+ 
+// Set URL Website dicoding.com
+let reachability = SCNetworkReachabilityCreateWithName(nil, "www.dicoding.com")
+ 
+var flags = SCNetworkReachabilityFlags()
+SCNetworkReachabilityGetFlags(reachability!, &flags)
+ 
+let isReachable = flags.contains(.reachable)
+print("IS REACHABLE: \(isReachable)")
