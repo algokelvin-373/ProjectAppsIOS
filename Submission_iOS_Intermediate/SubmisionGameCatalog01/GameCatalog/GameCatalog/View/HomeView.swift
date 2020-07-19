c46b3947a3b0ebc522d2b831e9dbb6943ed3e708
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-19 21:59:32 +0700

    Done to make TabBar SwiftUI

---
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/View/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  GameCatalog
+//
+//  Created by Kelvin HT on 7/19/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/View/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  GameCatalog
+//
+//  Created by Kelvin HT on 7/19/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
