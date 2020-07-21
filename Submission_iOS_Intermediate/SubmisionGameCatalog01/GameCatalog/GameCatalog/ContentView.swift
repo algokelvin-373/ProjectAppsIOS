04d77e61024cb13b43e5339ef1ccbaf5ff7cd893
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-21 21:56:39 +0700

    Set Asset TabBar and Good Display Asset

--- Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/ContentView.swift
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/ContentView.swift
@@ -17,12 +17,12 @@ struct ContentView: View {
             }).tag(0)
             
             SearchPageView().tabItem({
-            Image("\(ConstantVal.TabBarImage.tabBar0)")
+            Image("\(ConstantVal.TabBarImage.tabBar1)")
                 Text("\(ConstantVal.TabBarText.tabBar1)")
             }).tag(1)
             
             ProfileView().tabItem({
-            Image("\(ConstantVal.TabBarImage.tabBar0)")
+            Image("\(ConstantVal.TabBarImage.tabBar2)")
                 Text("\(ConstantVal.TabBarText.tabBar2)")
             }).tag(2)
         }
