11b887ad4001b5354424089795bdd34ab0e006df
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-21 22:13:51 +0700

    Add Module 22: Data API GET

---
+++ M22_Data_API/Data_GET/m22_data_api_01/m22_data_api_01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m22_data_api_01
+//
+//  Created by Kelvin HT on 7/21/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
