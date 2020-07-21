32ded82f54c71ceaab81529ca31753ef6d5ef9aa
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-21 11:08:10 +0700

    Done Add Biodata Profile + Type Text Roboto Regular, Medium, and Bold

--- Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/View/ProfileView.swift
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/View/ProfileView.swift
@@ -20,6 +20,17 @@ struct ProfileView: View {
                     .frame(width: 150.0, height: 150.0)
                     .offset(y: -50)
                     .padding(.bottom, -50)
+                
+                /**Name, Jobs, Company**/
+                Text("Kelvin Herwanda Tandrio")
+                    .font(.custom("Roboto-Bold", size: 30))
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                Text("Android Developer")
+                    .font(.custom("Roboto-Medium", size: 25))
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                Text("TSM Technology")
+                    .font(.custom("Roboto-Medium", size: 20))
+                    .frame(minWidth: 0, maxWidth: .infinity)
             }
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
