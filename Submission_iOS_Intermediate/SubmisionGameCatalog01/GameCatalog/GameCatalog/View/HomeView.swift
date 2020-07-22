72a8a78d8ff8ba70e55099bf4cfb8aba0f5e5a2c
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-23 00:46:01 +0700

    Success Make Item Data Game: Photos, Title, Rating, and Button Favorit ( No Complete )

--- Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/View/HomeView.swift
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/View/HomeView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home View")
+        ScrollView {
+            VStack() {
+                ForEach((1...10), id: \.self) { i in
+                    VStack(spacing: 20) {
+                        Image("background_profile")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        
+                        HStack{
+                            Text("Resident Evil")
+                                .font(.title)
+                            Spacer(minLength: 0)
+                            Image("ic-star-rating")
+                            Text("4.48/5")
+                            Button(action: {
+                            }) {
+                                Image("ic-love-on")
+                            }
+                        }.padding()
+                    }
+                    .background(Color(red: 255.0, green: 240.0, blue: 0.0, opacity: 1.0))
+                    .cornerRadius(5)
+                }
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                .background(Color.white)
+
+        }
     }
 }
 
