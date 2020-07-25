fd65fa80add3128e80a77343d00de60b47b90a36
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-25 19:23:49 +0700

    Add Module M22 part 2: Get Data API using SearchView

---
+++ M22_Data_API/Data_GET/m22_get_api_02/m22_get_api_02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m22_get_api_02
+//
+//  Created by Kelvin HT on 7/25/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
