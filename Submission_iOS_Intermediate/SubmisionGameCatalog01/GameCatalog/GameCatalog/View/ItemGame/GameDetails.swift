b1f062fe098536fdca2f3b1d9a9b8bccfdca4e6d
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-02 00:34:37 +0700

    Add New Page: GameDetails

---
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/View/ItemGame/GameDetails.swift
@@ -0,0 +1,21 @@
+//
+//  GameDetails.swift
+//  GameCatalog
+//
+//  Created by Kelvin HT on 8/2/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct GameDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        GameDetails()
+    }
+}
