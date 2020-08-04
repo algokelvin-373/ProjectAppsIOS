b539338f5e530c12e7d032858c3c5db5b89ece6d
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-04 15:28:29 +0700

    Add About Me

---
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/Model/ModelProfile.swift
@@ -0,0 +1,11 @@
+//
+//  ModelProfile.swift
+//  GameCatalog
+//
+//  Created by Kelvin HT on 8/4/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import Foundation
+
+var aboutMe = "My name is Kelvin Herwanda Tandrio. I graduated from the Department of Informatics at Universitas Sebelas Maret, Surakarta I was once a teaching assistant for the courses in Programming Concepts, Data Structures, and Numerical Methods.\n\nI’m working as an Android Developer in a company in Jakarta. I learned coding Android since I was in semester 5. I learn Android by taking various learning courses from basic to high-level implementation. Besides that, I also learn Image Processing and Machine Learning to get more knowledge."
