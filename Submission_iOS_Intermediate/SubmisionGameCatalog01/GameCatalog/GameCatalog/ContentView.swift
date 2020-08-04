bd565750fc5919c325b49c5fe773b786471be181
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-04 16:04:28 +0700

    Make Color TabSelected TabBar

--- Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/ContentView.swift
+++ Submission_iOS_Intermediate/SubmisionGameCatalog01/GameCatalog/GameCatalog/ContentView.swift
@@ -12,20 +12,20 @@ struct ContentView: View {
     var body: some View {
         TabView {
             HomeView().tabItem({
-            Image("\(ConstantVal.TabBarImage.tabBar0)")
+            Image("\(ConstantVal.TabBarImage.tabBar0)").renderingMode(.template)
                 Text("\(ConstantVal.TabBarText.tabBar0)")
             }).tag(0)
             
             SearchPageView().tabItem({
-            Image("\(ConstantVal.TabBarImage.tabBar1)")
+            Image("\(ConstantVal.TabBarImage.tabBar1)").renderingMode(.template)
                 Text("\(ConstantVal.TabBarText.tabBar1)")
             }).tag(1)
             
             ProfileView().tabItem({
-            Image("\(ConstantVal.TabBarImage.tabBar2)")
+            Image("\(ConstantVal.TabBarImage.tabBar2)").renderingMode(.template)
                 Text("\(ConstantVal.TabBarText.tabBar2)")
             }).tag(2)
-        }
+        }.accentColor(Color(red: 16.0/255, green: 152.0/255, blue: 251.0/255, opacity: 1.0))
     }
 }
 
