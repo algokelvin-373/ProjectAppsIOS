b4e936562e4f6e3cdc2593abb2792e74b67ae706
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-06 12:08:54 +0700

    Add TabBar FavoritePage

---
+++ Submission_iOS_Intermediate/ProyekAkhirGameCatalog/GameCatalog/GameCatalog/View/FavoritePageView.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritePageView.swift
+//  GameCatalog
+//
+//  Created by Kelvin HT on 8/6/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoritePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritePageView()
+    }
+}
--- Submission_iOS_Intermediate/ProyekAkhirGameCatalog/GameCatalog/GameCatalog/utils/ConstantVal.swift
+++ Submission_iOS_Intermediate/ProyekAkhirGameCatalog/GameCatalog/GameCatalog/utils/ConstantVal.swift
@@ -12,12 +12,14 @@ struct ConstantVal {
     struct TabBarImage {
         static let tabBar0 = "ic-home"
         static let tabBar1 = "ic-search-page"
-        static let tabBar2 = "ic-profile"
+        static let tabBar2 = "ic-favorite"
+        static let tabBar3 = "ic-profile"
     }
     
     struct TabBarText {
         static let tabBar0 = "Home"
         static let tabBar1 = "Search"
-        static let tabBar2 = "Profile"
+        static let tabBar2 = "Favorite"
+        static let tabBar3 = "Profile"
     }
 }
