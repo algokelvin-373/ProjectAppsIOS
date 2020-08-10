283d81c22652af4b230790d7abf7f1552360d9a5
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-11 01:17:40 +0700

    Make method to add data in database Core Data

--- M25_Database_Basic/m25_database_basic/m25_database_basic/PlayCoreData.swift
+++ M25_Database_Basic/m25_database_basic/m25_database_basic/PlayCoreData.swift
@@ -1,48 +1,35 @@
 //
-//  ContentView.swift
+//  PlayCoreData.swift
 //  m25_database_basic
 //
-//  Created by Kelvin HT on 8/7/20.
+//  Created by Kelvin HT on 8/10/20.
 //  Copyright © 2020 Kelvin HT. All rights reserved.
 //
 
+import Foundation
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    
-    @State var member:[NSManagedObject] = []
-    @State var newMemberString = ""
-    
-    var body: some View {
-        Text("Hello, World!")
-    }
-    
-    func addNewData() {
+class PlayCoreData {
+    func addNewData(id: Int, name: String, aboutName: String) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
         
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "Member", in: managedContext)!
-        
+
         let newMember = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        newMember.setValue(1, forKeyPath: "id")
-        newMember.setValue("Kelvin Herwanda Tandrio", forKeyPath: "name")
-        newMember.setValue("Android and iOS Developer", forKeyPath: "about")
+
+        newMember.setValue(id, forKey: "id")
+        newMember.setValue(name, forKey: "name")
+        newMember.setValue(aboutName, forKey: "about")
         
         do {
             try managedContext.save()
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
