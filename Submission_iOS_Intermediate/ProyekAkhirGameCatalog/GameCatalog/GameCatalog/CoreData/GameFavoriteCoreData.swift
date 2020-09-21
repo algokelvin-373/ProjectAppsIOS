1b5d99826167d6d79b80b8328e8e270c2a4bf4dd
kelvin373-ht <kelvin373.ht@gmail.com>
2020-09-21 14:59:27 +0700

    Add new Class 'GameFavoriteCoreData'

---
+++ Submission_iOS_Intermediate/ProyekAkhirGameCatalog/GameCatalog/GameCatalog/CoreData/GameFavoriteCoreData.swift
@@ -0,0 +1,68 @@
+//
+//  GameFavoriteCoreData.swift
+//  GameCatalog
+//
+//  Created by Kelvin HT on 9/21/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+import Combine
+
+class GameFavoriteCoreData: ObservableObject {
+    let managedContext: NSManagedObjectContext
+    @Published var data = [Member]()
+    
+    init() {
+        managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        readData()
+    }
+    
+    func addNewData(id: UUID, name: String, aboutName: String) {
+        let newMember = NSEntityDescription.insertNewObject(forEntityName: "Member", into: managedContext)
+
+        newMember.setValue(id, forKey: "id")
+        newMember.setValue(name, forKey: "name")
+        newMember.setValue(aboutName, forKey: "about")
+        
+        do {
+            try managedContext.save()
+            
+            print("Success input data")
+            
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    func readData() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Member")
+        
+        do {
+            guard let results = try managedContext.fetch(fetchRequest) as? [Member] else { return }
+            
+            self.data = results
+            
+            print("Read data success")
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    func deleteData(id: UUID) {
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Member")
+//
+//        fetchRequest.predicate = NSPredicate(format: "id == \(id)")
+//
+//        do {
+//            fetchRequest.fetchLimit = 1
+//            guard let result = try? managedContext.fetch(fetchRequest), let member = result.first as? Member else { return }
+//
+//            managedContext.delete(result)
+//
+//            try managedContext.save()
+//        } catch let error as NSError {
+//            print("Could not save. \(error), \(error.userInfo)")
+//        }
+    }
+}
