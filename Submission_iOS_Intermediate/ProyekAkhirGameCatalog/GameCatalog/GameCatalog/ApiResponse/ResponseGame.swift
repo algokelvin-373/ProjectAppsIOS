c6a9016b3250f5402868952b4dfd467073870b0f
kelvin373-ht <kelvin373.ht@gmail.com>
2020-09-23 23:40:20 +0700

    Success get data Game API But No Success for Search Game

--- Submission_iOS_Intermediate/ProyekAkhirGameCatalog/GameCatalog/GameCatalog/ApiResponse/ResponseGame.swift
+++ Submission_iOS_Intermediate/ProyekAkhirGameCatalog/GameCatalog/GameCatalog/ApiResponse/ResponseGame.swift
@@ -30,4 +30,26 @@ class ResponseGame {
                 
         }.resume()
     }
+    
+    /*Get Data Item Game from Search Data*/
+    func searchDataGame(key: String, completion: @escaping ([Games]) -> ()) {
+        let url = URLRequest(url: URL(string: "https://api.rawg.io/api/games?search=\(key)")!)
+            
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data2 = data, error == nil else {
+                return
+            }
+                
+            let jsonGame: DataGame?
+            do {
+                jsonGame = try JSONDecoder().decode(DataGame.self, from: data2)
+                completion(jsonGame!.results) // To set value Array<Game> in 'responseData'
+                print(jsonGame!.results)
+            }
+            catch {
+                print("Failed to convert \(error.localizedDescription)")
+            }
+                
+        }.resume()
+    }
 }
