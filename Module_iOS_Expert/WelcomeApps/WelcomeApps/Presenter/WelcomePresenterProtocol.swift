e103a88456d08e345b8d366ada517b19215d1ea3
kelvin373-ht <kelvin373.ht@gmail.com>
2020-11-14 22:58:44 +0700

    iOS Expert Project 01: VIPER Architecture - Presenter Component

---
+++ Module_iOS_Expert/WelcomeApps/WelcomeApps/Presenter/WelcomePresenterProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomePresenterProtocol.swift
+//  WelcomeApps
+//
+//  Created by Kelvin HT on 11/14/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import Foundation
+
+protocol WelcomePresenterProtocol: class {
+    func getMessage(name: String) -> MessageEntity
+}
