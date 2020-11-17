ac60bf68c9978ad183bee70798a895eae8a96a62
kelvin373-ht <kelvin373.ht@gmail.com>
2020-11-17 14:06:21 +0700

    RxSwift-RegisterApps: Add new project 'RegisterApps'

---
+++ Module_iOS_Expert/RxSwiftTutorial/AppsRegisterWithRxSwift/RegisterRxSwift/RegisterRxSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RegisterRxSwift
+//
+//  Created by Kelvin HT on 11/17/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
