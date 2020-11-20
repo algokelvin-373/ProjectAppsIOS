68f0eaa197098a78acde062571aff77f828a79e8
kelvin373-ht <kelvin373.ht@gmail.com>
2020-11-20 19:56:03 +0700

    RxSwift Tutorial - RxSwiftSubject: Publish Subject

--- Module_iOS_Expert/RxSwiftTutorial/RxSwiftSubject/RxSwiftSubject/main.swift
+++ Module_iOS_Expert/RxSwiftTutorial/RxSwiftSubject/RxSwiftSubject/main.swift
@@ -7,6 +7,27 @@
 //
 
 import Foundation
+import RxSwift
 
-print("Hello, World!")
+print("Subject - Publish Subject v01")
+let sourcePublish = PublishSubject<Int>()
+sourcePublish.subscribe(onNext: { print("Subscription value is \($0)") })
+sourcePublish.onNext(1)
+sourcePublish.onNext(2)
+sourcePublish.subscribe(onNext: { print("Another subscription value is \($0)") })
+sourcePublish.onNext(3)
+sourcePublish.onCompleted()
 
+print("\nSubject - Publish Subject v02")
+let sourcePublish02 = PublishSubject<Int>()
+sourcePublish02.subscribe(onNext: { print("Subscription value is \($0)") })
+sourcePublish02.onNext(1)
+sourcePublish02.onNext(2)
+sourcePublish02.subscribe(onNext: { print("Another subscription value is \($0)") })
+sourcePublish02.onNext(3)
+sourcePublish02.onNext(4)
+sourcePublish02.subscribe(onNext: { print("Another again subscription value is \($0)") })
+sourcePublish02.onNext(5)
+sourcePublish02.onCompleted()
+
+print("")
