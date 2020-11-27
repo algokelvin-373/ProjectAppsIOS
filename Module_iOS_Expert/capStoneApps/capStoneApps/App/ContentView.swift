9c7b9aa733f9c3ecd187849f1cf54101eacf5e96
kelvin373-ht <kelvin373.ht@gmail.com>
2020-11-27 23:10:35 +0700

    Submission iOS Expert CapstoneApps (e-Hobby): Create Page Movie, Sport, Game, Travel, and Profile

--- Module_iOS_Expert/capStoneApps/capStoneApps/App/ContentView.swift
+++ Module_iOS_Expert/capStoneApps/capStoneApps/App/ContentView.swift
@@ -10,7 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            HomeView().tabItem({
+            Image("\(ConstantVal.TabBarImage.homeTabBar)").renderingMode(.template)
+                Text("\(ConstantVal.TabBarText.homeTabBar)")
+            }).tag(0)
+            
+            SearchPageView().tabItem({
+            Image("\(ConstantVal.TabBarImage.searchTabBar)").renderingMode(.template)
+                Text("\(ConstantVal.TabBarText.searchTabBar)")
+            }).tag(1)
+            
+            FavoritePageView().tabItem({
+            Image("\(ConstantVal.TabBarImage.favoriteTabBar)").renderingMode(.template)
+                Text("\(ConstantVal.TabBarText.favoriteTabBar)")
+            }).tag(2)
+            
+            ProfileView().tabItem({
+            Image("\(ConstantVal.TabBarImage.profileTabBar)").renderingMode(.template)
+                Text("\(ConstantVal.TabBarText.profileTabBar)")
+            }).tag(3)
+        }.accentColor(Color(red: 16.0/255, green: 152.0/255, blue: 251.0/255, opacity: 1.0))
     }
 }
 
---
+++ Module_iOS_Expert/capStoneApps/capStoneApps/View/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  CapStoneApps
+//
+//  Created by Kelvin HT on 11/27/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile Page")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
