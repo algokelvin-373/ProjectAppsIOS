5c2d7fc717e154e3c78e4ba91b11d37a267c736b
kelvin373-ht <kelvin373.ht@gmail.com>
2020-12-03 01:34:18 +0700

    MealsApps - VIPER: Implement VIPER Architecture

---
+++ Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/View/MealView.swift
@@ -0,0 +1,21 @@
+//
+//  MealView.swift
+//  MealsApps
+//
+//  Created by Kelvin HT on 12/3/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct MealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealView()
+    }
+}
