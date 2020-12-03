79358d2c2dfcb1beb7045d11217128f6082a2fd8
kelvin373-ht <kelvin373.ht@gmail.com>
2020-12-03 23:32:02 +0700

    MealsApps - VIPER: Add Component 'Model' in MealApps

---
+++ Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/Entity/Model/MealModel.swift
@@ -0,0 +1,16 @@
+//
+//  MealModel.swift
+//  MealsApps
+//
+//  Created by Kelvin HT on 12/3/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import Foundation
+
+struct MealModel: Equatable, Identifiable {
+  let idMeal: String
+  let titleMeal: String
+  let imageMeal: String
+  let descriptionMeal: String
+}
--- Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/Entity/Model/MealResponse.swift
+++ Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/Entity/Model/MealResponse.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+struct MealResponse: Decodable {
+  let meals: [Meals]
+}
+
+struct Meals: Decodable {
+  private enum CodingKeys: String, CodingKey {
+    case idMeal = "idCategory"
+    case titleMeal = "strCategory"
+    case imageMeal = "strCategoryThumb"
+    case descriptionMeal = "strCategoryDescription"
+  }
+
+  let idMeal: String?
+  let titleMeal: String?
+  let imageMeal: String?
+  let descriptionMeal: String?
+}
