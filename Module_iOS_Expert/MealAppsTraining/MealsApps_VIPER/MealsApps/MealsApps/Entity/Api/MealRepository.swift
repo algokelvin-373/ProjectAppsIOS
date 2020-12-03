02d6c42ffad25833e07465a59417888c9f3e7ea7
kelvin373-ht <kelvin373.ht@gmail.com>
2020-12-04 00:49:06 +0700

    MealsApps - VIPER: Create 'MealRepository' using implement Protocol - Interactor && Mapper Data

--- Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/Entity/Api/MealRepository.swift
+++ Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/Entity/Api/MealRepository.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+final class MealRepository: NSObject {
+    typealias MealInstance = (MealDataSource) -> MealRepository
+
+    fileprivate let remote: MealDataSource
+
+    private init(remote: MealDataSource) {
+        self.remote = remote
+    }
+
+    static let sharedInstance: MealInstance = { remoteRepo in
+        return MealRepository(remote: remoteRepo)
+    }
+}
+
+extension MealRepository: MealRepositoryProtocol {
+    func getCategories(result: @escaping (Result<[CategoryModel], Error>) -> Void) {
+        self.remote.getCategories { remoteResponses in
+          switch remoteResponses {
+          case .success(let categoryResponses):
+            let resultList = CategoryMapper.mapCategoryResponsesToDomains(input: categoryResponses)
+            result(.success(resultList))
+          case .failure(let error):
+            result(.failure(error))
+          }
+        }
+    }
+}
