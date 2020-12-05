3f071abe75595d57eb18c0f6185b3db6d74d0049
kelvin373-ht <kelvin373.ht@gmail.com>
2020-12-05 22:46:18 +0700

    MealsApps - VIPER: Create 'MealPresenter' for Bussiness Logic Meal Apps

--- Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/Presenter/MealPresenter.swift
+++ Module_iOS_Expert/MealAppsTraining/MealsApps_VIPER/MealsApps/MealsApps/Presenter/MealPresenter.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class MealPresenter: ObservableObject {
+    private let mealUseCase: MealUseCaseProtocol
+
+    @Published var categories: [CategoryModel] = []
+    @Published var errorMessage: String = ""
+    @Published var loadingState: Bool = false
+    
+    init(mealUseCase: MealUseCaseProtocol) {
+      self.mealUseCase = mealUseCase
+    }
+    
+    func getCategories() {
+        loadingState = true
+        mealUseCase.getCategories { result in
+            switch result {
+            case .success(let categories):
+              DispatchQueue.main.async {
+                self.loadingState = false
+                self.categories = categories
+              }
+            case .failure(let error):
+              DispatchQueue.main.async {
+                self.loadingState = false
+                self.errorMessage = error.localizedDescription
+              }
+            }
+        }
+    }
+}
