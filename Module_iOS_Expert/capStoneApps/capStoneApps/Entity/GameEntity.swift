5d18137142f7014579b68585f9c4b98e27276add
kelvin373-ht <kelvin373.ht@gmail.com>
2020-12-11 01:03:38 +0700

    Submission iOS Expert CapstoneApps (e-Hobby): Game Page - Done Component 'Entiity' (Model, DataSource, Repository)

---
+++ Module_iOS_Expert/capStoneApps/capStoneApps/Entity/GameEntity.swift
@@ -0,0 +1,37 @@
+//
+//  GameEntity.swift
+//  CapStoneApps
+//
+//  Created by Kelvin HT on 12/10/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import Foundation
+
+struct DataGame: Decodable {
+    let results: [Games]
+}
+
+struct Games: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case idGame = "id"
+        case nameGame = "name"
+        case backgroundImageGame = "background_image"
+        case releasedGame = "released"
+        case ratingGame = "rating"
+    }
+
+    let idGame: Int?
+    let nameGame: String?
+    let backgroundImageGame: String?
+    let releasedGame: String?
+    let ratingGame: Float?
+}
+
+struct GameModel: Equatable, Identifiable {
+    let id: Int
+    let name: String
+    let backgroundImage: String
+    let released: String
+    let rating: Float
+}
