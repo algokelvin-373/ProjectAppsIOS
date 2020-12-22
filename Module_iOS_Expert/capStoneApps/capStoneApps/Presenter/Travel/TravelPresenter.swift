055d4fb7a5f20ed5a6af9e4ec1e5ed16c60a777f
kelvin373-ht <kelvin373.ht@gmail.com>
2020-12-22 23:29:05 +0700

    Submission iOS Expert CapstoneApps (e-Hobby): Travel Page - Create and Implement VIPER for Travel Page

---
+++ Module_iOS_Expert/capStoneApps/capStoneApps/Presenter/Travel/TravelPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  TravelPresenter.swift
+//  CapStoneApps
+//
+//  Created by Kelvin HT on 12/22/20.
+//  Copyright © 2020 Kelvin HT. All rights reserved.
+//
+
+import Foundation
+
+class TravelPresenter: ObservableObject {
+    
+}
