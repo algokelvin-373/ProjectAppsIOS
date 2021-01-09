4dac351ba4431c4d18a80e59b0d6b4baa83f3251
kelvin373-ht <kelvin373.ht@gmail.com>
2021-01-10 00:38:51 +0700

    Submission iOS Expert CapstoneApps (e-Hobby): Game Page - Desain UI Item Menu Game Part 1

--- Module_iOS_Expert/capStoneApps/capStoneApps/View/Game/GameRowsView.swift
+++ Module_iOS_Expert/capStoneApps/capStoneApps/View/Game/GameRowsView.swift
@@ -7,15 +7,45 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct GameRowsView: View {
+
+    var dataGames: GameModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            WebImage(url: URL(string: dataGames.backgroundImage))
+                .resizable()
+                .indicator(.activity)
+                .transition(.fade(duration: 0.5))
+                .scaledToFit()
+                .frame(width: 200)
+                .cornerRadius(30)
+                .padding(.top)
+
+            VStack(alignment: .leading, spacing: 10) {
+                Text(dataGames.name)
+                    .font(.title)
+                    .bold()
+
+                Text(dataGames.released)
+                    .font(.system(size: 14))
+                    .lineLimit(2)
+            }.padding(EdgeInsets(top: 0, leading: 16, bottom: 16, trailing: 16))
+        }.frame(width: UIScreen.main.bounds.width - 32, height: 250)
+        .background(Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1)))
+        .cornerRadius(30)
     }
 }
 
 struct GameRowsView_Previews: PreviewProvider {
     static var previews: some View {
-        GameRowsView()
+        GameRowsView(dataGames: GameModel(
+            id: 1,
+            name: "Grand Theft Auto V",
+            backgroundImage: "https://media.rawg.io/media/games/84d/84da2ac3fdfc6507807a1808595afb12.jpg",
+            released: "2013-09-17",
+            rating: 4.48))
     }
 }
