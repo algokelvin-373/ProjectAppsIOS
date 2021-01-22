cc67f77422dc2e49abed4aadc2e0090f8244435c
kelvin373-ht <kelvin373.ht@gmail.com>
2021-01-22 07:43:34 +0700

    Test Driven Development: Create Example Project TriangleAppTesting

---
+++ Module_iOS_Expert/TestDrivenDevelopment/TriangleAppTesting/TriangleAppTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TriangleAppTesting
+//
+//  Created by Kelvin HT on 1/22/21.
+//  Copyright © 2021 Kelvin HT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
